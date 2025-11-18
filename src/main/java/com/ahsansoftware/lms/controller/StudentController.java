7c507c5ed9ae08e025633ceb6d04ebd588b4ebf2
Ahsan Shahzad <ahsanshahzadpakcelestial@gmail.com>
2025-11-18 09:25:22 +0500

    Learning Java Backend Day 2 (Forgot to push lately)

--- src/main/java/com/ahsansoftware/lms/controller/StudentController.java
+++ src/main/java/com/ahsansoftware/lms/controller/StudentController.java
@@ -1,5 +1,28 @@
 package com.ahsansoftware.lms.controller;
 
+import com.ahsansoftware.lms.model.Student;
+import com.ahsansoftware.lms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Students")
 public class StudentController {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping
+    public Optional<Student> getStudentById(@PathVariable long id){
+        return studentRepository.findById(id);
+    }
 }
