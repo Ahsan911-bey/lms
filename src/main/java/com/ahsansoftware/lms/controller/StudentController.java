dedcf002532b6c5412fa5b5b95744f5fd7f812b2
Ahsan Shahzad <ahsanshahzadpakcelestial@gmail.com>
2025-11-19 10:10:44 +0500

    Learning Java Backend Day 4 (Forgot to Push lately)

--- src/main/java/com/ahsansoftware/lms/controller/StudentController.java
+++ src/main/java/com/ahsansoftware/lms/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/Students")
+@RequestMapping("/students")
 public class StudentController {
 
     @Autowired
@@ -21,8 +21,26 @@ public class StudentController {
         return studentRepository.findAll();
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     public Optional<Student> getStudentById(@PathVariable long id){
         return studentRepository.findById(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable long id , @RequestBody Student studentDetails){
+        Student student = studentRepository.findById(id).orElseThrow();
+        student.setEmail(studentDetails.getEmail());
+        student.setName(studentDetails.getName());
+        return studentRepository.save(student);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable long id){
+        studentRepository.deleteById(id);
+        return "Student Deleted with id: "+id;
     }
 }
